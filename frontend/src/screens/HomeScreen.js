bdcf48f96368201ff2880dc7cfa2d32f6849957d
Brian Archibald <brianallenarchibald@gmail.com>
2021-02-13 18:33:05 -0600

    Refactor: Start homescreen and images

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import teachers from '../teachers'
+
+const HomeScreen = props => {
+  return(
+    <div>
+      <h1>Recent Teachers</h1>
+      <Row>
+      </Row>
+    </div>
+  )
+}
+
+export default HomeScreen
