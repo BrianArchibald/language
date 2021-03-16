dfe357d16e4a03df04470a8c4744dbb0cba1cce1
Brian Archibald <brianallenarchibald@gmail.com>
2021-03-16 14:30:53 -0500

    Refactor: started teacher model

--- backend/base/models.py
+++ backend/base/models.py
@@ -2,5 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Teacher(models.Model):
-    
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=False)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    flag = models.CharField(max_length=200, null=True, blank=True)
+    rate = models.DecimalField(max_digits=7, decimal_places=2)
+    # image =
+    teachingLanguage = models.CharField(max_length=200, null=True, blank=True)
+    speakingLanguage = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2)
+    reviews =
+    numLessons = models.IntegerField(null=True, blank=True, default=0)
+    description = models.TextField(null=True, blank=True)
+    video =
+    bookTeacher =
+    calendarPreview =
+    messageTeacher =
+    online =
+    _id =
 
