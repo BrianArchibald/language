5929bec907cc32c8eba5f48d22ed40af06265c1a
Brian Archibald <brianallenarchibald@gmail.com>
2021-03-17 15:54:16 -0500

    Refactor: Migrated first model, added to admin

--- backend/base/models.py
+++ backend/base/models.py
@@ -2,21 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Teacher(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     flag = models.CharField(max_length=200, null=True, blank=True)
-    rate = models.DecimalField(max_digits=7, decimal_places=2)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     # image =
     teachingLanguage = models.CharField(max_length=200, null=True, blank=True)
     speakingLanguage = models.CharField(max_length=200, null=True, blank=True)
-    rating = models.DecimalField(max_digits=7, decimal_places=2)
-    reviews =
+    rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     numLessons = models.IntegerField(null=True, blank=True, default=0)
     description = models.TextField(null=True, blank=True)
-    video =
-    bookTeacher =
-    calendarPreview =
-    messageTeacher =
-    online =
-    _id =
+    # video =
+    online = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
 
