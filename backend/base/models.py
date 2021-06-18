b244684db47141932cb95d17820f625d9a467bbb
Brian Archibald <brianallenarchibald@gmail.com>
2021-06-18 16:23:00 -0500

    Refactor: added orderitem and registered models to admin

--- backend/base/models.py
+++ backend/base/models.py
@@ -42,4 +42,18 @@ class Order(models.Model):
     createdAt = models.DateTimeField(auto_now_add=False)
     _id = models.AutoField(primary_key=True, editable=False)
 
+    def __str__(self):
+        return str(self.createdAt)
+
+class OrderItem(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    # image =
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
 
